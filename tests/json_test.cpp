7cf1af45cb9af5af3da69ffbcf9dd9481e4ee62c
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-19 16:22:14 +0300

    Update json_test.cpp

--- tests/json_test.cpp
+++ tests/json_test.cpp
@@ -46,7 +46,7 @@ TEST(Testfunction,  typeCheck1) {
    // cout << s1.avg.type().name() <<endl;
    // cout << s1.debt.type().name() <<endl;
     EXPECT_EQ(any_cast<s1.group.type()>(s1.group),  31);
-    EXPECT_EQ(any_cast<int>(s1.avg),  4);
+    EXPECT_EQ(any_cast<s1.avg.type()>(s1.avg),  4);
     EXPECT_EQ(any_cast<string>(s1.debt),  "C++");
     EXPECT_TRUE(true); }
 nlohmann::json jst3 = {
