dafae677a48f34db9deb8f9a2f36643dc1c34796
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-21 23:08:57 +0300

    Update boostf.cpp

--- sources/boostf.cpp
+++ sources/boostf.cpp
@@ -6,9 +6,11 @@ std::vector<std::string> recdir(std::string path_to_ftp) {
     boost::filesystem::recursive_directory_iterator end;
     std::string dir;
     std::vector<std::string> ret;
-    int countv=0;
-    for (boost::filesystem::recursive_directory_iterator it(path_to_ftp); it != end; ++it) { //перебор файлов и каталогов
-        boost::filesystem::file_status fs = boost::filesystem::status(*it); //информация о файле
+    int countv = 0;
+    for (boost::filesystem::recursive_directory_iterator 
+         it(path_to_ftp); it != end; ++it) { //перебор файлов и каталогов
+        boost::filesystem::file_status
+            fs = boost::filesystem::status(*it); //информация о файле
         switch (fs.type()) { //
         case boost::filesystem::regular_file:
             break;
@@ -22,7 +24,8 @@ std::vector<std::string> recdir(std::string path_to_ftp) {
             break;
         }
         std::string fi = it->path().stem().string();
-        if (!boost::algorithm::ends_with(fi, ".old") && boost::algorithm::starts_with(fi, "balance_")) { //
+        if (!boost::algorithm::ends_with(fi, ".old")
+            && boost::algorithm::starts_with(fi, "balance_")) { //
             countv++;
             std::string brok = dir + " " + it->path().stem().string();
             Broker::AddBroker(brok);
@@ -33,14 +36,13 @@ std::vector<std::string> recdir(std::string path_to_ftp) {
     return ret;
 }
 
-
 void show_vector(std::vector<std::string> vec) {
-    for (std::vector<std::string>::iterator it = vec.begin(); it != vec.end(); ++it)
+    for (std::vector<std::string>::iterator
+         it = vec.begin(); it != vec.end(); ++it)
         std::cout << *it << std::endl;
 }
 void show_broker(std::vector<Broker> vec) {
     for (Broker b : vec) {
         std::cout << b.toString() << std::endl;
     }
-
 }
