5a4fc585501403a7f8df6ae5228aadb2c8296fcd
legik777 <75944488+legik777@users.noreply.github.com>
2021-02-28 13:18:23 +0300

    Create main.hpp

---
+++ include/main.hpp
@@ -0,0 +1,49 @@
+template <typename T>
+class Stack
+{
+private:
+    struct Node
+    {
+        Node* next; //указатель на следующий элемент списка
+        T obj; //хранит данные
+    };
+    
+
+public:   
+    Node* nodeHead;
+    size_t length;
+    Stack() {
+        nodeHead = NULL;
+        length = 0;
+    };
+    void push(T&& value) { //push - передаём rvalue сслыку на объект
+        Node* nd = new Node;
+        nd->obj = std::move(value);
+        nd->next = nodeHead;
+        ++length;
+        nodeHead = std::move(nd);
+    }
+    void push(const T& value) { //push - передаём lvalue сслыку на объект
+        Node* nd = new Node;
+        nd->obj = std::move(value);
+        nd->next = nodeHead;
+        ++length;
+        nodeHead = nd;
+    }
+
+    size_t Length() {
+        return length;
+    }
+    void pop() { //снимаем верхний элемент стэка (удаляем)
+        if (length > 0){
+        T tmp = std::move(nodeHead->obj);
+        nodeHead = std::move(nodeHead->next);
+        length--;
+        }
+    }
+    const T& head() const { //возвращает ссылку на шапку стэка
+        T& ref = nodeHead->obj;
+        return ref;
+    }
+};
+
