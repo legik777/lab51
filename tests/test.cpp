8ff5dfde720a12ca4ef81876b18f51dd8e56c04e
legik777 <75944488+legik777@users.noreply.github.com>
2021-03-03 23:29:06 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -32,7 +32,7 @@ TEST(class_Stack, Test1) {
 }
 
 TEST(class_Stack, Test2) {
-   /* Stack<MyClass> obj1;
+    Stack<MyClass> obj1;
     MyClass x1;
     MyClass x2(10, "lala");
     MyClass x3(11, "hihi");
@@ -50,7 +50,7 @@ TEST(class_Stack, Test2) {
     EXPECT_EQ(obj1.Length(), 1);
     EXPECT_EQ(obj1.head().GetName(), "EMPTY");
     EXPECT_EQ(obj1.head().GetValue(), 0);
-    void del();*/
+    void del();
 }
 
 TEST(class_Stack_2, Test1) {
