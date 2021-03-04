6d848c0456cb8916e01e97f3cf071dca296c8087
legik777 <75944488+legik777@users.noreply.github.com>
2021-03-04 11:44:23 +0300

    Update main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -34,8 +34,8 @@ public:
         nd->next = nodeHead;
         ++length;
         nodeHead = nd;
-        //nd = NULL;
-        //delete nd;
+        nd = NULL;
+        delete nd;
     }
 
     size_t Length() {
