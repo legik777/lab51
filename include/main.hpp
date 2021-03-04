be12d9f2429953b7122305ee5b8e23876b51c2e7
legik777 <75944488+legik777@users.noreply.github.com>
2021-03-04 10:55:49 +0300

    Update main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -22,22 +22,22 @@ public:
     ~Stack() {
         //void del();
     }
-    void del() {
+    /*void del() {
         while (nodeHead) {
             Node* tmp = nodeHead;
             nodeHead = nodeHead->next;
             delete tmp;
             void del();
        }
-    }
+    }*/
     void push(T&& value) { //push - передаём rvalue сслыку на объект
         Node* nd = new Node;
         nd->obj = std::move(value);
         nd->next = nodeHead;
         ++length;
         nodeHead = std::move(nd);
-        nd = NULL;
-        delete nd;
+        //nd = NULL;
+        //delete nd;
     }
     void push(const T& value) { //push - передаём lvalue сслыку на объект
         if(Node* nd = new Node(value)){
